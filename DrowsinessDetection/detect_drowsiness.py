b8e3e4bd4efc729ef2118a2dc96dd933d9f4a121
Prashant <prashant.brahmbhatt32@outlook.com>
2019-03-17 15:48:25 +0530

    added the drowsiness script

---
+++ DrowsinessDetection/detect_drowsiness.py
@@ -0,0 +1,140 @@
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import playsound
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+def sound_alarm(path):
+    '''Takes in the path of the sound to play, mp3/WAV'''
+    playsound.playsound(path)
+
+def eye_aspect_ratio(eye):
+    '''Taking the array of eye coordinates and returning the ratio'''
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    
+    ear = (A + B)/ (2 * C)
+    
+    return ear
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+    help="path to facial landmark predictor")
+ap.add_argument("-a", "--alarm", type=str, default="",
+    help="path alarm .WAV file")
+ap.add_argument("-w", "--webcam", type=int, default=0,
+    help="index of webcam on system")
+args = vars(ap.parse_args())
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 48  # decreasing it will cause the detector to be more sensitive
+ 
+# initialize the frame counter as well as a boolean used to
+# indicate if the alarm is going off
+
+COUNTER = 0
+ALARM_ON = False
+
+print("Loading up the facial landmark detector")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+print("setting up Video Stream...")
+vs = VideoStream(src=args["webcam"]).start()
+time.sleep(1.0)
+ 
+# loop over frames from the video stream
+while True:
+	# grab the frame from the threaded video file stream, resize
+	# it, and convert it to grayscale
+	# channels)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# detect faces in the grayscale frame
+	rects = detector(gray, 0)
+
+	# loop over the face detections
+	for rect in rects:
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+
+		# extract the left and right eye coordinates, then use the
+		# coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
+
+		# compute the convex hull for the left and right eye, then
+		# visualize each of the eyes
+		rightEyeHull = cv2.convexHull(rightEye)
+		leftEyeHull = cv2.convexHull(leftEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+		# check to see if the eye aspect ratio is below the blink
+		# threshold, and if so, increment the blink frame counter
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
+
+			# if the eyes were closed for a sufficient number of
+			# then sound the alarm
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				# if the alarm is not on, turn it on
+				if not ALARM_ON:
+					ALARM_ON = True
+
+					# check to see if an alarm file was supplied,
+					# and if so, start a thread to have the alarm
+					# sound played in the background
+					if args["alarm"] != "":
+						t = Thread(target=sound_alarm,
+							args=(args["alarm"],))
+						t.deamon = True
+						t.start()
+
+				# draw an alarm on the frame
+				cv2.putText(frame, "!!!Sleep Alert!!!", (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+		# otherwise, the eye aspect ratio is not below the blink
+		# threshold, so reset the counter and alarm
+		else:
+			COUNTER = 0
+			ALARM_ON = False
+
+		# draw the computed eye aspect ratio on the frame to help
+		# with debugging and setting the correct eye aspect ratio
+		# thresholds and frame counters
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+ 
+	# show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
